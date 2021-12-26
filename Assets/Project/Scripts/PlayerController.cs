065e7b89dcb03f6973f082daca44d0e83c066b4e
LamenLuan <luan.leme.s35@gmail.com>
2021-12-25 23:33:00 -0300

    Changing the rule to update rigid body velocity

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -22,13 +22,14 @@ public class PlayerController : MonoBehaviour
 
     private void MovePlayer(float horizontalMove)
     {
-        // Changing player sprite direction if horizontalMove changed
-        if(horizontalMove != 0) _spriteRenderer.flipX = horizontalMove < 0;
+        if (horizontalMove != 0) {
+            // Changing player sprite direction if horizontalMove changed
+            _spriteRenderer.flipX = horizontalMove < 0;
 
+            float y = _rigidBody.velocity.y;
+            _rigidBody.velocity = new Vector2(horizontalMove * _speed, y);
+        }
         _animator.SetBool("IsWalking", _rigidBody.velocity.x != 0);
-        
-        float y = _rigidBody.velocity.y;
-        _rigidBody.velocity = new Vector2(horizontalMove * _speed, y);
     }
 
 }
